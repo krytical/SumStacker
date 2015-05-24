4aa860b43700e9788210ec2514de617c79740a4c
Nbalsh <brandon.bn.nelson@gmail.com>
2015-05-24 14:53:02 -0700

    Evan's box and prefab

--- SumStacker/Assets/Scripts/Boxes.cs
+++ SumStacker/Assets/Scripts/Boxes.cs
@@ -3,28 +3,26 @@ using System.Collections;
 
 public class Boxes : MonoBehaviour
 {
-
-    float fall = 0;
-    public static int gridWeight = 5;
-    public static int gridHeight = 10;
-    public static Transform[,] grid = new Transform[gridWeight, gridHeight];
+	
+	float fall = 0;
+	public static int gridWeight = 5;
+	public static int gridHeight = 10;
 	public bool isHeld = false;
-
-    //private bool canMove = true;
-
-    void Start()
-    {
-        // check if game over
+	
+	public int boxValues = 0;
+	
+	void Start()
+	{
+		// check if game over
 		if (!isValidPosition()  && !isHeld)
-        {
-            Application.LoadLevel(0);
-            Destroy(gameObject);
-        }
-        //canMove = true;
-    }
-
-    void Update()
-    {
+		{
+			Application.LoadLevel(0);
+			Destroy(gameObject);
+		}
+	}
+	
+	void Update()
+	{
 		if (!isHeld) {
 			//RIGHT KEY
 			if (Input.GetKeyDown (KeyCode.RightArrow) || Input.GetKeyDown (KeyCode.D)) {
@@ -36,8 +34,8 @@ public class Boxes : MonoBehaviour
 				} else
 					transform.position += new Vector3 (-1, 0, 0);
 			}
-        //LEFT KEY
-        else if (Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.A)) {
+			//LEFT KEY
+			else if (Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.A)) {
 				Vector2 vOld = round (transform.position);
 				transform.position += new Vector3 (-1, 0, 0);
 				if (isValidPosition ()) {
@@ -46,12 +44,33 @@ public class Boxes : MonoBehaviour
 				} else
 					transform.position += new Vector3 (1, 0, 0);
 			}
+			//DOWN KEY
+			else if (Input.GetKeyDown (KeyCode.DownArrow) ||
+			         Time.time - fall >= 1 || Input.GetKeyDown (KeyCode.S)) {
+				Vector2 vOld = round (transform.position);
+				transform.position += new Vector3 (0, -1, 0);
+				if (isValidPosition ()) {
+					UpdateBlock (vOld);
+				} else {
+					transform.position += new Vector3 (0, 1, 0);
+					Grid.deleteCompleteRowsAndDrop ();
+					FindObjectOfType<GameController> ().blockNum++;
+					FindObjectOfType<SpawnBox> ().SpawnNewBox ();
+					enabled = false;
+				}
+				
+				fall = Time.time;
+			} else if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.A) ||
+			           Input.GetKeyUp (KeyCode.RightArrow) || Input.GetKeyUp (KeyCode.D)) {
+				GameObject.Find ("Main Camera").GetComponent<AudioSource> ().enabled = false;
+			}
+			//SHIFT KEY
 			else if(Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)){
 				Boxes[] boxArray = FindObjectsOfType(typeof(Boxes)) as Boxes[];
 				foreach(Boxes b in boxArray){
 					if(b.isHeld){
 						Vector2 v = round(transform.position);
-						grid[(int)v.x, (int)v.y] = b.transform;
+						Grid.grid[(int)v.x, (int)v.y] = b.transform;
 						b.transform.position = round(transform.position);
 						transform.position = new Vector3 (7, 0, 0);
 						b.isHeld = false;
@@ -60,59 +79,39 @@ public class Boxes : MonoBehaviour
 					}
 				}
 			}
-        //DOWN KEY
-        else if (Input.GetKeyDown (KeyCode.DownArrow) ||
-				Time.time - fall >= 1 || Input.GetKeyDown (KeyCode.S)) {
-				Vector2 vOld = round (transform.position);
-				transform.position += new Vector3 (0, -1, 0);
-				if (isValidPosition ()) {
-					UpdateBlock (vOld);
-				} else {
-					transform.position += new Vector3 (0, 1, 0);
-					//Grid.deleteCompleteRowsAndDrop ();
-					FindObjectOfType<GameController> ().blockNum++;
-					FindObjectOfType<SpawnBox> ().SpawnNewBox ();
-					enabled = false;
-				}
-
-				fall = Time.time;
-			} else if (Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.A) ||
-				Input.GetKeyUp (KeyCode.RightArrow) || Input.GetKeyUp (KeyCode.D)) {
-				GameObject.Find ("Main Camera").GetComponent<AudioSource> ().enabled = false;
-			} 
+		}
+		
 	}
-
-    }
-
-    // Moves a block to a new position
-    public void UpdateBlock(Vector2 vOld)
-    {
-        grid[(int)vOld.x, (int)vOld.y] = null;
-        Vector2 v = round(transform.position);
-        grid[(int)v.x, (int)v.y] = transform;
-    }
-
-    // checks if spot to move to is valid
-    bool isValidPosition()
-    {
-        Vector2 v = round(transform.position);
-        if (!isInsideGrid(v) || grid[(int)v.x, (int)v.y] != null)
-            return false;
-        return true;
-    }
-
-    // Rounds a vector to the nearest int
-    public static Vector2 round(Vector2 v)
-    {
-        return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
-    }
-
-    // Checks if the position is in the grid
-    public static bool isInsideGrid(Vector2 pos)
-    {
-        return ((int)pos.x >= 0 &&
-         (int)pos.x < gridWeight &&
-         (int)pos.y >= 0);
-    }
-
+	
+	// Moves a block to a new position
+	public void UpdateBlock(Vector2 vOld)
+	{
+		Grid.grid[(int)vOld.x, (int)vOld.y] = null;
+		Vector2 v = round(transform.position);
+		Grid.grid[(int)v.x, (int)v.y] = transform;
+	}
+	
+	// checks if spot to move to is valid
+	bool isValidPosition()
+	{
+		Vector2 v = round(transform.position);
+		if (!isInsideGrid(v) || Grid.grid[(int)v.x, (int)v.y] != null)
+			return false;
+		return true;
+	}
+	
+	// Rounds a vector to the nearest int
+	public static Vector2 round(Vector2 v)
+	{
+		return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
+	}
+	
+	// Checks if the position is in the grid
+	public static bool isInsideGrid(Vector2 pos)
+	{
+		return ((int)pos.x >= 0 &&
+		        (int)pos.x < gridWeight &&
+		        (int)pos.y >= 0);
+	}
+	
 }
--- SumStacker/Assets/Scripts/Grid.cs
+++ SumStacker/Assets/Scripts/Grid.cs
@@ -3,90 +3,87 @@ using System.Collections;
 
 public class Grid : MonoBehaviour
 {
-
-    public static int gridWeight = 5;
-    public static int gridHeight = 10;
-    public static int goaltotal = 10;
-	public static int gridLoseHeight = 8;
-    public static Transform[,] grid = new Transform[gridWeight, gridHeight];
-
-    // Use this for initialization
-    void Start() { }
-
-    // Update is called once per frame
-    void Update() { }
-
-    // Deletes all objects in a row
-    public static void DeleteRow(int y)
-    {
-        for (int x = 0; x < gridWeight; ++x)
-        {
-            Destroy(grid[x, y].gameObject);
-            grid[x, y] = null;
-        }
-    }
-
-    // Drops all objects in a row
-    public static void RowDown(int y)
-    {
-        for (int x = 0; x < gridWeight; ++x)
-        {
-            if (grid[x, y] != null)
-            {
-                grid[x, y - 1] = grid[x, y];
-                grid[x, y] = null;
-                grid[x, y - 1].position += new Vector3(0, -1, 0);
-            }
-        }
-    }
-
-    // Drops all rows from y up
-    public static void RowDownAll(int y)
-    {
-        for (int i = y; i < gridHeight; ++i)
-            RowDown(i);
-    }
-
-    // Checks if a row is full
-    public static bool isRowFull(int y)
-    {
-        for (int x = 0; x < gridWeight; ++x)
-            if (grid[x, y] == null)
-                return false;
-        return true;
-    }
-
-    // deletes complete rows and drops all boxes above row
-    public static void deleteCompleteRowsAndDrop()
-    {
-        for (int y = 0; y < gridHeight; ++y)
-        {
-            if (isRowFull(y) && correctRowSum(y))    //TODO Does the row have to be full for it to be deleted?
-            {
-                DeleteRow(y);
-                RowDownAll(y + 1);
-                --y;
-            }
-        }
-    }
-
-    // Checks if row sums up to correct value
-    public static bool correctRowSum(int y) //TODO make this work
-    {
-
-        int currenttotal = 0;
-        for (int x = 0; x < gridWeight; ++x)
-        {
-            currenttotal++; //adds the total of the box values at grid[x, y]
-        }
-        if (currenttotal == goaltotal)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
+	
+	public static int gridWeight = 5;
+	public static int gridHeight = 10;
+	public static int goaltotal = 10;
+	public static Transform[,] grid = new Transform[gridWeight, gridHeight];
+	
+	private static int rowBoxValue = 0;
+	
+	// Use this for initialization
+	void Start() { }
+	
+	// Update is called once per frame
+	void Update() { }
+	
+	// Deletes all objects in a row
+	public static void DeleteRow(int y)
+	{
+		for (int x = 0; x < gridWeight; ++x)
+		{
+			if (grid[x,y] != null) {
+				Destroy(grid[x, y].gameObject);
+				grid[x, y] = null;
+			}
+		}
+	}
+	
+	// Drops all objects in a row
+	public static void RowDown(int y)
+	{
+		for (int x = 0; x < gridWeight; ++x)
+		{
+			if (grid[x, y] != null)
+			{
+				grid[x, y - 1] = grid[x, y];
+				grid[x, y] = null;
+				grid[x, y - 1].position += new Vector3(0, -1, 0);
+			}
+		}
+	}
+	
+	// Drops all rows from y up
+	public static void RowDownAll(int y)
+	{
+		for (int i = y; i < gridHeight; ++i)
+			RowDown(i);
+	}
+	
+	// deletes complete rows and drops all boxes above row
+	public static void deleteCompleteRowsAndDrop()
+	{;
+		for (int y = 0; y < gridHeight; ++y)
+		{
+			if (correctRowSum(y))
+			{
+				print ("FUCK YEAH");
+				//DeleteRow(y);
+				// RowDownAll(y + 1);
+				--y;
+			}
+		}
+	}
+	
+	// Checks if row sums up to correct value
+	public static bool correctRowSum(int y) //TODO make this work
+	{		
+		int currenttotal = 0;
+		for (int x = 0; x < gridWeight; x++)
+		{
+			if (grid[x,y] != null){
+				GameObject currbox = grid[x,y].gameObject;
+				rowBoxValue = currbox.GetComponent<Boxes>().boxValues;
+				currenttotal += rowBoxValue;
+			}
+		}
+		if (currenttotal == goaltotal)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
