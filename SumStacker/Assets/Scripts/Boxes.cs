284a7e61fa1089da185d68fe0ff5c9b6ab14c088
EWaterman <evanwaterman@hotmail.com>
2015-05-24 11:01:37 -0700

    fixed grid and position

--- SumStacker/Assets/Scripts/Boxes.cs
+++ SumStacker/Assets/Scripts/Boxes.cs
@@ -9,17 +9,17 @@ public class Boxes : MonoBehaviour
     public static int gridHeight = 8;
     public static Transform[,] grid = new Transform[gridWeight, gridHeight];
 
-    private bool canMove = true;
+    //private bool canMove = true;
 
-    void Start() 
-    { 
+    void Start()
+    {
         // check if game over
         if (!isValidPosition())
         {
             Application.LoadLevel(0);
             Destroy(gameObject);
         }
-        canMove = true;
+        //canMove = true;
     }
 
     void Update()
@@ -27,11 +27,11 @@ public class Boxes : MonoBehaviour
         //RIGHT KEY
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
-            //Vector2 vOld = round(transform.position);
+            Vector2 vOld = round(transform.position);
             transform.position += new Vector3(1, 0, 0);
             if (isValidPosition())
             {
-                UpdateBlock();
+                UpdateBlock(vOld);
             }
             else
                 transform.position += new Vector3(-1, 0, 0);
@@ -39,11 +39,11 @@ public class Boxes : MonoBehaviour
         //LEFT KEY
         else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
-            //Vector2 vOld = round(transform.position);
+            Vector2 vOld = round(transform.position);
             transform.position += new Vector3(-1, 0, 0);
             if (isValidPosition())
             {
-                UpdateBlock();
+                UpdateBlock(vOld);
             }
             else
                 transform.position += new Vector3(1, 0, 0);
@@ -52,17 +52,17 @@ public class Boxes : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.DownArrow) ||
                 Time.time - fall >= 1 || Input.GetKeyDown(KeyCode.S))
         {
-            //Vector2 vOld = round(transform.position);
+            Vector2 vOld = round(transform.position);
             transform.position += new Vector3(0, -1, 0);
             if (isValidPosition())
             {
-                UpdateBlock();
+                UpdateBlock(vOld);
             }
             else
             {
                 transform.position += new Vector3(0, 1, 0);
                 Grid.deleteCompleteRowsAndDrop();
-				FindObjectOfType<GameController>().blockNum++;
+                FindObjectOfType<GameController>().blockNum++;
                 FindObjectOfType<SpawnBox>().SpawnNewBox();
                 enabled = false;
             }
@@ -70,48 +70,22 @@ public class Boxes : MonoBehaviour
             fall = Time.time;
         }
     }
-    /*
-        // Moves a block to a new position
-        public static void UpdateBlock(Vector2 vOld) 
-        {
-            grid[(int)vOld.x, (int)vOld.y] = null;
-            Vector2 v = round(transform.position);
-            grid[(int)v.x, (int)v.y] = tansform;
-        }
-    */
 
     // Moves a block to a new position
-    void UpdateBlock() // TODO there must be a better way to do this considering all blocks are only a single piece
+    public void UpdateBlock(Vector2 vOld)
     {
-        for (int y = 0; y < gridHeight; ++y)
-            for (int x = 0; x < gridWeight; ++x)
-                if (grid[x, y] != null)
-                    if (grid[x, y].parent == transform)
-                        grid[x, y] = null;
-        foreach (Transform child in transform)
-        {
-            Vector2 v = round(child.position);
-            grid[(int)v.x, (int)v.y] = child;
-        }
+        grid[(int)vOld.x, (int)vOld.y] = null;
+        Vector2 v = round(transform.position);
+        grid[(int)v.x, (int)v.y] = transform;
     }
 
     // checks if spot to move to is valid
-    bool isValidPosition() //TODO needs fixing
+    bool isValidPosition()
     {
-        if (canMove)
-        {
-            foreach (Transform child in transform)
-            {
-                Vector2 v = round(child.position);
-                if (!isInsideGrid(v))
-                    return false;
-                if (grid[(int)v.x, (int)v.y] != null &&
-                      grid[(int)v.x, (int)v.y].parent != transform)
-                    return false;
-            }
-            return true;
-        }
-        return false;
+        Vector2 v = round(transform.position);
+        if (!isInsideGrid(v) || grid[(int)v.x, (int)v.y] != null)
+            return false;
+        return true;
     }
 
     // Rounds a vector to the nearest int
@@ -126,17 +100,6 @@ public class Boxes : MonoBehaviour
         return ((int)pos.x >= 0 &&
          (int)pos.x < gridWeight &&
          (int)pos.y >= 0);
-    }
-
-    // TODO remove these if possible or put collision fields back in 
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        canMove = false;
-
-    }
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        canMove = false;
     }
 
 }
