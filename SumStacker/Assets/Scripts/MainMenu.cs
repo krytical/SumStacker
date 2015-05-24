e82878bb389b875c88b1a367da44993c3904d725
krytical <johnnie-l@Live.ca>
2015-05-23 23:50:41 -0700

    Main Menu

---
+++ SumStacker/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public GameObject[] boxPrefabs;
+	
+	public float spawnDelay = 3f;
+	float nextSpawn = 1f;
+	
+	public float initialVelocity = 2f;
+
+	GameObject bGO;
+
+	public void StartGame() {
+		Application.LoadLevel ("Game");
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+
+	void Update () {
+		nextSpawn -= Time.deltaTime;
+		
+		if (nextSpawn <= 0) {
+			nextSpawn = spawnDelay;
+			bGO = (GameObject)Instantiate (boxPrefabs [Random.Range (0, boxPrefabs.Length)], transform.position, transform.rotation * Quaternion.Euler (0, 0, 90));
+			bGO.GetComponent<Rigidbody2D> ().velocity = transform.rotation * new Vector2 (initialVelocity, 0);
+		}		
+	}
+
+}
